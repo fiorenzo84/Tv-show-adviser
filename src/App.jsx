c0b8637d2009f5dc1016fe2522cd9470b5da0d98
Florent Tuppo <florent.tuppo@gmail.com>
2023-01-06 16:06:07 +0100

    react project

---
+++ src/App.jsx
@@ -0,0 +1,90 @@
+import { useState, useEffect } from "react";
+import { TVShowAPI } from "./api/tv-show";
+import { Logo } from "./components/Logo/Logo";
+import { TVShowDetail } from "./components/TVShowDetail/TVShowDetail";
+import { BACKDROP_BASE_URL } from "./config";
+import "./global.css";
+import s from "./style.module.css";
+import logo from "./assets/images/logo.png";
+// import { TVShowListItem } from "./components/TVShowListItem/TVShowListItem";
+import { TVShowList } from "./components/TVShowList/TVShowList";
+import { SearchBar } from "./components/SearchBar/SearchBar";
+
+export function App() {
+  const [currentTVShow, setCurrentTVShow] = useState();
+  const [recommendationList, setRecommendationList] = useState([]);
+
+  async function fetchPopulars() {
+    const populars = await TVShowAPI.fetchPopulars();
+    if (populars.length > 0) {
+      setCurrentTVShow(populars[0]);
+    }
+  }
+
+  async function fetchRecommendations(tvShowId) {
+    const recommendations = await TVShowAPI.fetchRecommendations(tvShowId);
+    if (recommendations.length > 0) {
+      setRecommendationList(recommendations.slice(0, 10));
+    }
+  }
+
+  useEffect(() => {
+    fetchPopulars();
+  }, []);
+
+  useEffect(() => {
+    if (currentTVShow) {
+      fetchRecommendations(currentTVShow.id);
+    }
+  }, [currentTVShow]);
+
+  // function setCurrentTvShowFromRecommendation(tvShow) {
+  //   alert(JSON.stringify(tvShow));
+  // }
+
+  // console.log(currentTVShow);
+  //console.log(recommendationList);
+
+  async function searchTVShow(tvShowName) {
+    const searchResponse = await TVShowAPI.fetchByTitle(tvShowName);
+    if (searchResponse.length > 0) {
+      setCurrentTVShow(searchResponse[0]);
+    }
+  }
+
+  return (
+    <div
+      className={s.main_container}
+      style={{
+        background: currentTVShow
+          ? `linear-gradient(rgba(0,0,0,0.55), rgba(0,0,0,0.55)), url("${BACKDROP_BASE_URL}${currentTVShow.backdrop_path}") no-repeat center / cover`
+          : "black",
+      }}>
+      <div className={s.header}>
+        <div className="row">
+          <div className="col-4">
+            <Logo
+              image={logo}
+              title="Watowatch"
+              subtitle="Find a show you may like"
+            />
+          </div>
+          <div className="col-sm-12 col-md-4">
+            <SearchBar onSubmit={searchTVShow} />
+          </div>
+        </div>
+      </div>
+      <div className={s.tv_show_detail}>
+        {currentTVShow && <TVShowDetail tvShow={currentTVShow} />}
+      </div>
+      <div className={s.recommended_shows}>
+        {recommendationList && recommendationList.length > 0 && (
+          <TVShowList
+            onClickItem={setCurrentTVShow}
+            tvShowList={recommendationList}
+          />
+        )}
+      </div>
+    </div>
+  );
+}
---
+++ src/config.js
@@ -0,0 +1,6 @@
+const BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY_PARAM = "?api_key=c2dd901ec007cc87b540c1c31b997517";
+const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original/";
+const SMALL_IMG_COVER_BASE_URL = "https://image.tmdb.org/t/p/w300/";
+
+export { BASE_URL, API_KEY_PARAM, BACKDROP_BASE_URL, SMALL_IMG_COVER_BASE_URL };
